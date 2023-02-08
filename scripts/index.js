c7715aed91c90b69d846368764575721bea7c2f5
enoch <85976396+MM4096@users.noreply.github.com>
2023-02-08 21:06:53 +1300

    Changed firebase method to Node.js

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+import {initializeApp} from "firebase/app"
+import "firebase/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCgebjp9UWGlH-gMBp0MVYJ8thoXqglt-Q",
+    authDomain: "chatcentral-89222.firebaseapp.com",
+    databaseURL: "https://chatcentral-89222-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "chatcentral-89222",
+    storageBucket: "chatcentral-89222.appspot.com",
+    messagingSenderId: "1019016139576",
+    appId: "1:1019016139576:web:0a0717c37921a1389da7cd"
+  };
+
+initializeApp(firebaseConfig);
+const db = firebase.database();
