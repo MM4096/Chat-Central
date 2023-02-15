4cc0e86422d18d15d9cde9ffd2dbbf881716714c
enoch <85976396+MM4096@users.noreply.github.com>
2023-02-15 16:32:32 +1300

    Able to get chat user is part of

--- scripts/chatData.js
+++ scripts/chatData.js
@@ -1,36 +1,58 @@
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js"
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js"
 import { getDatabase, ref, child, get } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCgebjp9UWGlH-gMBp0MVYJ8thoXqglt-Q",
-    authDomain: "chatcentral-89222.firebaseapp.com",
-    databaseURL: "https://chatcentral-89222-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "chatcentral-89222",
-    storageBucket: "chatcentral-89222.appspot.com",
-    messagingSenderId: "1019016139576",
-    appId: "1:1019016139576:web:0a0717c37921a1389da7cd"
-  };
+	apiKey: "AIzaSyCgebjp9UWGlH-gMBp0MVYJ8thoXqglt-Q",
+	authDomain: "chatcentral-89222.firebaseapp.com",
+	databaseURL: "https://chatcentral-89222-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "chatcentral-89222",
+	storageBucket: "chatcentral-89222.appspot.com",
+	messagingSenderId: "1019016139576",
+	appId: "1:1019016139576:web:0a0717c37921a1389da7cd"
+};
 
 initializeApp(firebaseConfig);
 const db = getDatabase();
 const auth = getAuth();
 const dbRef = ref(db);
+let user;
 
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-
-    } else {
-        window.location.href = "login.html";
-    }
+onAuthStateChanged(auth, (_user) => {
+	if (_user) {
+		user = _user;
+	} else {
+		window.location.href = "login.html";
+	}
 });
 
-get(child(dbRef, "users/")).then((snapshot) => {
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-    } else {
-      console.log("No data available");
-    }
-}).catch((error) => {
-    console.error(error);
+
+$(document).ready(function () {
+	get(child(dbRef, "chats/")).then((snapshot) => {
+		let currentUid = user.uid;
+		snapshot.forEach((childSnapshot) => {
+			let key = childSnapshot.key;
+			key = key.split(",");
+			// see if user is part of the chat
+			if (key.indexOf(currentUid) !== -1) {
+				let buttonText = "";
+				// finding other users in the chat
+				for (let i = 0; i < key.length; i++) {
+					if (i !== key.indexOf(currentUid)) {
+						get(child(dbRef, "users/" + key[i])).then((_snapshot) => {
+							console.log(_snapshot.val().username);
+							if (buttonText != "") {
+								buttonText += ", ";
+							}
+							buttonText += _snapshot.val().username;
+							$("#userList").append('<button chatId="' + childSnapshot.key + '">' + buttonText + '</button>');
+						})
+					}
+				}
+			}
+		});
+	})
+		.catch((error) => {
+			console.log(error);
+		})
 });
